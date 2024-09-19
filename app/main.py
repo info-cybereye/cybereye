f3ca5f2015ee3bfe71fe9a4247e04a26927af990
Cyber Eye <info.cybereye.acg@gmail.com>
2024-09-19 11:25:54 +0600

    main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 from bs4 import BeautifulSoup
 import requests
@@ -25,3 +26,8 @@ def search_keyword():
         results.append(title.get_text())
 
     return jsonify(results)
+if __name__ == '__main__':
+    # Get the port from the environment variable or default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    # Run the app on host 0.0.0.0 and the specified port
+    app.run(debug=True, host='0.0.0.0', port=port)
